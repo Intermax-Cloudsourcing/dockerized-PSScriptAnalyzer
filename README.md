7b104fc19cd4f25d429facafa41ceb9bfd5f8599
wilmardo <info@wilmardenouden.nl>
2018-09-24 14:13:47 +0200

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # dockerized-PSScriptAnalyzer
-`strace -f -e open /opt/microsoft/powershell/6/pwsh 2>&1 | grep -v "/opt/"`
--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/opt/microsoft/powershell/6/pwsh -C 'Set-PSReadLineOption -HistorySaveStyle SaveNothing'
-opt/microsoft/powershell/6/pwsh
-exec "$@"
